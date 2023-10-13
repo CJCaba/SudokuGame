86462fa2687ac3949a500389f802714aa0fe4b91
cabansa3 <cabansa3@msu.edu>
2023-10-12 20:31:52 -0400

    Create GameView class and fill window with white background

--- GameLib/GameView.h
+++ GameLib/GameView.h
@@ -8,6 +8,8 @@
 #ifndef ARES_GAMELIB_GAMEVIEW_H
 #define ARES_GAMELIB_GAMEVIEW_H
 
+//#include "Game.h"
+
 /**
  * View class for our game
  */
@@ -20,6 +22,18 @@ private:
     // void OnLeftUp(wxMouseEvent &event);
     // void OnTimer(wxTimerEvent &event);
     void OnPaint(wxPaintEvent &event);
+
+    /// An object that describes our game
+    // Game mGame;
+
+    /// Timer allowing for animation
+    wxTimer mTimer;
+
+    /// Stopwatch used to measure elapsed time
+    wxStopWatch mStopWatch;
+
+    /// The last stopwatch time
+    long mTime = 0;
 
 public:
     void Initialize(wxFrame* parent);
