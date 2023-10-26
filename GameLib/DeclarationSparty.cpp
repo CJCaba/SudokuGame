0540cf316d676df5b7d8beb00167c0aa7035df01
Daniel Flanagan <danflanman@icloud.com>
2023-10-25 20:38:30 -0400

    Fixed a type in the line    node->GetAttribute("mouth-pivot-x", "0").ToDouble(&mMouthPivotX); previously was     node->GetAttribute("mouth-pivot=x", "0").ToDouble(&mMouthPivotX);

--- GameLib/DeclarationSparty.cpp
+++ GameLib/DeclarationSparty.cpp
@@ -18,7 +18,7 @@ void DeclarationSparty::XmlLoad(wxXmlNode *node)
     mImage1 = node->GetAttribute("image1").ToStdWstring();
     mImage2 = node->GetAttribute("image2").ToStdWstring();
     node->GetAttribute("mouth-pivot-angle", "0").ToDouble(&mMouthPivotAngle);
-    node->GetAttribute("mouth=pivot-x", "0").ToDouble(&mMouthPivotX);
+    node->GetAttribute("mouth-pivot-x", "0").ToDouble(&mMouthPivotX);
     node->GetAttribute("mouth-pivot-y", "0").ToDouble(&mMouthPivotY);
     node->GetAttribute("target-x", "0").ToDouble(&mTargetX);
     node->GetAttribute("target-y", "0").ToDouble(&mTargetY);
