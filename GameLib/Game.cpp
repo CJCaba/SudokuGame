f56f5f15fa0fb013055fdd76cdabb4f9c562d368
OkayJerry <geraldjhoskinsiii@gmail.com>
2023-10-29 17:54:29 -0400

    Resolved nullptr Sparty issue

--- GameLib/Game.cpp
+++ GameLib/Game.cpp
@@ -182,7 +182,7 @@ void Game::OnLeftDown(wxMouseEvent &event)
     double virtualX = ( event.GetX() - mXOffset ) / mScale;
     double virtualY = ( event.GetY() - mYOffset ) / mScale;
 
-    if (WithinWidth(virtualX) and WithinHeight(virtualY))
+    if (mSparty and WithinWidth(virtualX) and WithinHeight(virtualY))
         mSparty->MoveToPoint( wxPoint(virtualX, virtualY) );
 }
 /**
